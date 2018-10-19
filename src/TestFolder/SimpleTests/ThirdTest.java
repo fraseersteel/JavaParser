3781b94c9f613e9c2e50ef08444ba3eaa389f151
fraser steel <frasersteel@btinternet.com>
2018-10-19 10:29:36 +0100

    some new testing stuff

---
+++ src/TestFolder/SimpleTests/ThirdTest.java
@@ -0,0 +1,22 @@
+package TestFolder.SimpleTests;
+
+public class ThirdTest {
+
+    int x = 0;
+
+    public int helloWorld(){
+        int i = 0;
+        int a = 2;
+        int b = 3;
+
+         a=i;
+         a=i*a;
+         b=a*i;
+         return a;
+    }
+
+    public void test(){
+        int y = 1;
+        System.out.println(helloWorld()*y);
+    }
+}
