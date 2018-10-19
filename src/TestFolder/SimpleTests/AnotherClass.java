19ad24d2d104bc144984800783ad198db9dac305
Fraser Steel <cwb15142@strath.ac.uk>
2018-10-19 10:23:35 +0100

    adding test classes

---
+++ src/TestFolder/SimpleTests/AnotherClass.java
@@ -0,0 +1,30 @@
+package TestFolder.SimpleTests;
+
+public class AnotherClass {
+
+
+    int u = 10;
+
+    public int helloWorld(){
+        int i = 0;
+        int a = 2;
+        int y = 7;
+
+        a = i;
+        a = i*a;
+
+        if(y>12021){
+
+        }
+
+        if(a>1232){
+            int t = 12;
+          int r= 12;
+        }
+        return a;
+    }
+
+    public void test(){
+        System.out.println(helloWorld());
+    }
+}
